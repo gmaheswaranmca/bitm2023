d3c77dd63c14f2764805918eb433c4b45ebc69d7
gmaheswaranmca <gmaheswaranmca@gmail.com>
2023-08-18 14:06:33 +0530

    "notes"

---
+++ b2/vcs.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define SIZE 100000
+int main
+{
+	int N, M, K;
+	int A[SIZE], B[SIZE];
+	
+	scanf("%d%d%d",&N,&M,&K);
+	
+	for(int I = 0; I < M; I++){
+		scanf("%d",&A[I]);
+	}
+	for(int I = 0; I < K; I++){
+		scanf("%d",&B[I]);
+	}
+}
+
