34bc6f8153216dcfc12d15452e34b4afdefe2995
gmaheswaranmca <gmaheswaranmca@gmail.com>
2023-08-18 11:30:58 +0530

    "notes"

---
+++ b6/bears-candies-codechef.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void solve(){
+	int A, B; // candies on hand
+	scanf("%d%d",&A,&B);
+	int Limak=0,Bob=0;
+	int I = 1;
+	while(Limak <= A && Bob <= B){
+		if(I % 2 == 1){
+			Limak += I;
+		}
+		else{
+			Bob += I;
+		}
+		I++;
+	}
+	
+	if(Limak <= A){
+		printf("Limak");
+	}
+	else if(Bob <= B){
+		printf("Bob");
+	}
+}
+int main(){
+	int T;
+	scanf("%d",&T);
+	while(T--){
+		solve();
+	}
+	
+}
